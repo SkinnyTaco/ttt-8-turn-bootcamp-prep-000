a9ad68483ba494173c4fb8510601e278b71e3fa6
Daniel Dawson <dannyldawson@gmail.com>
2018-11-12 01:12:37 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,7 +41,7 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index)
-    display_board(board,index)
+    display_board(board)
   else
     puts "I'm sorry, that is not a valid move."
     turn(board)
