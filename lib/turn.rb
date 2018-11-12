0134fb414cc809778fe2fe8edd90acd87d47efb2
Daniel Dawson <dannyldawson@gmail.com>
2018-11-12 00:36:27 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,3 @@
-def display_board(board)
-  
-end
 
 def valid_move?(board, index)
   if index < board.length
@@ -20,4 +17,20 @@ def position_taken?(board, index)
   else 
     false
   end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+end
+
+def move(board, index, character = "X")
+  board[index] = character
 end
