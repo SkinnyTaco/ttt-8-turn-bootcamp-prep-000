7cc5fa4d4c5983e9ff718570dc33a0cc0373cc71
Daniel Dawson <dannyldawson@gmail.com>
2018-11-12 00:34:06 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,23 @@
 def display_board(board)
   
 end
+
+def valid_move?(board, index)
+  if index < board.length
+    if position_taken?(board, index)
+      false
+    else
+      true
+    end
+  else 
+    false 
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  else 
+    false
+  end
+end
